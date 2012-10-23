7246de2a284babbc2a6e8338a251d3afacde5e10
Billy Reisinger <billy.reisinger@gmail.com>
2012-10-22 22:35:47 -0500

    move executable into settings file

--- XMLLint.py
+++ XMLLint.py
@@ -1,14 +1,13 @@
-import commands
+import commands, subprocess
 import sublime, sublime_plugin
 
 class XmllintCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     self.view.run_command("save")
     filename=self.view.file_name()
-
-    xml = commands.getoutput("xmllint --format " + filename)
+    executable = self.view.settings().get("xmllint_path")
+    xml = commands.getoutput(executable + " --format " + filename)
 
     if len(xml) > 0:
       self.view.replace(edit, sublime.Region(0, self.view.size()), xml.decode('utf-8'))
       self.view.set_syntax_file('Packages/XML/XML.tmLanguage')
-      #sublime.set_timeout(self.save, 100)
